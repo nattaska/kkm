63b31d6b6827baec613f7f69fe8518ab1547285c
nattaska <natt.ska@gmail.com>
2019-04-27 16:27:39 +0700

    Loading

--- views/checkin/js/default.js
+++ views/checkin/js/default.js
@@ -5,6 +5,7 @@
         var module = "checkin";    
         var arrEmp;    
     
+        $('#loader').show();
         $.post(module+"/xhrSearch", function(o) {
             arrEmp = o;
             $("#listTimeChecked").html("");
@@ -16,67 +17,156 @@
                             +'  <th>'+o[i].timout+'</th> '
                             +'</tr>');
             }
+            $('#loader').hide();
         }, 'json');
-
         
         $("#clocked").submit(function(e) {
             e.preventDefault();
             $('#loader').show();
-
-            $("#code").val('0');
-            for (var i=0; i<arrEmp.length; i++) {
-                if ($("#phone").val() == arrEmp[i].phone) {
-                    // console.log(arrEmp[i]);
-                    $("#code").val(arrEmp[i].code);
-                    (arrEmp[i].timin === '-')?$("#clocktype").val('in'):$("#clocktype").val('out');
+        }).validate({
+            rules: {
+                phone: {
+                    required: true,
+                    minlength: 10
                 }
-            }
+            },
+            // Specify validation error messages
+            messages: {
+                phone: {
+                    required: "Please enter your phone number",
+                    minlength: "Your phone must be at least 10 characters long"
+                }
+            },
+            submitHandler: function(form) {
+                var url = $(form).attr('action');
+                var data = $(form).serialize();
 
-            if ($("#code").val() === '0') {
-                $("#msgMain").html('<div class="alert alert-warning"><button type="button" class="close">×</button><strong>Warning!</strong> Phone number does not matched !!!</div>');
-                // alert("Phone number does not matched !!!");
-            } else {                
+                $("#code").val('0');
 
-                var url = $(this).attr('action');
-                var data = $(this).serialize();
-
-                $.post(url, data, function(oc) {
-                    
-                    if (oc.res > 0) {
-                        $("#msgMain").html('<div class="alert alert-success"><button type="button" class="close">×</button><strong>Success!</strong> Code '+$("#code").val()+' already checked</div>');
-
-                        $.post(module+'/xhrSearch', function(o) {
-                            arrEmp = o;
-                            $("#listTimeChecked").html("");
-                            for (var i=0; i<o.length; i++) {
-                                $('#listTimeChecked').append('<tr>	'
-                                            +'  <th scope="row">'+o[i].code+'</th> ' 
-                                            +'  <th>'+o[i].name+'</th> ' 
-                                            +'  <th>'+o[i].timin+'</th> '
-                                            +'  <th>'+o[i].timout+'</th> '
-                                            +'</tr>');
-                            }
-                        }, 'json');
-                    } else {
-                        $("#msgMain").html('<div class="alert alert-danger"><button type="button" class="close">×</button><strong>Error!</strong> '+oc.error+'</div>');
+                for (var i=0; i<arrEmp.length; i++) {
+                    if ($("#phone").val() == arrEmp[i].phone) {
+                        // console.log(arrEmp[i]);
+                        $("#code").val(arrEmp[i].code);
+                        (arrEmp[i].timin === '-')?$("#clocktype").val('in'):$("#clocktype").val('out');
                     }
-                }, 'json');
-            }
-            $('#loader').hide();
-            //timing the alert box to close after 5 seconds
-            window.setTimeout(function () {
-                $(".alert").fadeTo(500, 0).slideUp(500, function () {
-                    $(this).remove();
-                });
-            }, 5000);
+                }
     
-            //Adding a click event to the 'x' button to close immediately
-            $('.alert .close').on("click", function (e) {
-                $(this).parent().fadeTo(500, 0).slideUp(500);
-            }); 
+                if ($("#code").val() === '0') {
+                    $("#msgMain").html('<div class="alert alert-warning"><button type="button" class="close">×</button><strong>Warning!</strong> Phone number does not matched !!!</div>');
+                    $('#loader').hide();
+                } else {
+    
+                    $.post(url, data, function(oc) {
+                        
+                        if (oc.res > 0) {
+                            $("#msgMain").html('<div class="alert alert-success"><button type="button" class="close">×</button><strong>Success!</strong> Code '+$("#code").val()+' already checked</div>');
+    
+                            $.post(module+'/xhrSearch', function(o) {
+                                arrEmp = o;
+                                $("#listTimeChecked").html("");
+                                for (var i=0; i<o.length; i++) {
+                                    $('#listTimeChecked').append('<tr>	'
+                                                +'  <th scope="row">'+o[i].code+'</th> ' 
+                                                +'  <th>'+o[i].name+'</th> ' 
+                                                +'  <th>'+o[i].timin+'</th> '
+                                                +'  <th>'+o[i].timout+'</th> '
+                                                +'</tr>');
+                                }
+                            }, 'json');
+                        } else {
+                            $("#msgMain").html('<div class="alert alert-danger"><button type="button" class="close">×</button><strong>Error!</strong> '+oc.error+'</div>');
+                        }
+                        $('#loader').hide();
+                        // $('#loader').hide();
+                        //timing the alert box to close after 5 seconds
+                        window.setTimeout(function () {
+                            $(".alert").fadeTo(500, 0).slideUp(500, function () {
+                                $(this).remove();
+                            });
+                        }, 5000);
+                
+                        //Adding a click event to the 'x' button to close immediately
+                        $('.alert .close').on("click", function (e) {
+                            $(this).parent().fadeTo(500, 0).slideUp(500);
+                        }); 
+                    }, 'json');
+                }
+                // $('#loader').hide();
+                //timing the alert box to close after 5 seconds
+                window.setTimeout(function () {
+                    $(".alert").fadeTo(500, 0).slideUp(500, function () {
+                        $(this).remove();
+                    });
+                }, 5000);
+        
+                //Adding a click event to the 'x' button to close immediately
+                $('.alert .close').on("click", function (e) {
+                    $(this).parent().fadeTo(500, 0).slideUp(500);
+                }); 
 
-            return false;
+                return false;  //This doesn't prevent the form from submitting.
+            }
         });
+
+        
+        // $("#clocked").submit(function(e) {
+        //     e.preventDefault();
+        //     $('#loader').show();
+
+        //     $("#code").val('0');
+        //     for (var i=0; i<arrEmp.length; i++) {
+        //         if ($("#phone").val() == arrEmp[i].phone) {
+        //             // console.log(arrEmp[i]);
+        //             $("#code").val(arrEmp[i].code);
+        //             (arrEmp[i].timin === '-')?$("#clocktype").val('in'):$("#clocktype").val('out');
+        //         }
+        //     }
+
+        //     if ($("#code").val() === '0') {
+        //         $("#msgMain").html('<div class="alert alert-warning"><button type="button" class="close">×</button><strong>Warning!</strong> Phone number does not matched !!!</div>');
+        //         // alert("Phone number does not matched !!!");
+        //     } else {                
+
+        //         var url = $(this).attr('action');
+        //         var data = $(this).serialize();
+
+        //         $.post(url, data, function(oc) {
+                    
+        //             if (oc.res > 0) {
+        //                 $("#msgMain").html('<div class="alert alert-success"><button type="button" class="close">×</button><strong>Success!</strong> Code '+$("#code").val()+' already checked</div>');
+
+        //                 $.post(module+'/xhrSearch', function(o) {
+        //                     arrEmp = o;
+        //                     $("#listTimeChecked").html("");
+        //                     for (var i=0; i<o.length; i++) {
+        //                         $('#listTimeChecked').append('<tr>	'
+        //                                     +'  <th scope="row">'+o[i].code+'</th> ' 
+        //                                     +'  <th>'+o[i].name+'</th> ' 
+        //                                     +'  <th>'+o[i].timin+'</th> '
+        //                                     +'  <th>'+o[i].timout+'</th> '
+        //                                     +'</tr>');
+        //                     }
+        //                 }, 'json');
+        //             } else {
+        //                 $("#msgMain").html('<div class="alert alert-danger"><button type="button" class="close">×</button><strong>Error!</strong> '+oc.error+'</div>');
+        //             }
+        //         }, 'json');
+        //     }
+        //     $('#loader').hide();
+        //     //timing the alert box to close after 5 seconds
+        //     window.setTimeout(function () {
+        //         $(".alert").fadeTo(500, 0).slideUp(500, function () {
+        //             $(this).remove();
+        //         });
+        //     }, 5000);
+    
+        //     //Adding a click event to the 'x' button to close immediately
+        //     $('.alert .close').on("click", function (e) {
+        //         $(this).parent().fadeTo(500, 0).slideUp(500);
+        //     }); 
+
+        //     return false;
+        // });
 
     });
   
