d2dfda7cd32160af9d1dcec0933a2917ba869234
nattaska <natt.ska@gmail.com>
2020-03-19 15:52:50 +0700

    Use https

--- config/paths.php
+++ config/paths.php
@@ -1,7 +1,7 @@
 <?php
 
 // define('URL', 'http://localhost/kkm/');
-define('URL', 'http://kkmstaff.herokuapp.com/kkm/');
+define('URL', 'https://kkmstaff.herokuapp.com/kkm/');
 define('NT_ORG', 'AS17552 True Internet');
 define('NT_HOST', 'asianet.co.th');
 date_default_timezone_set("Asia/Bangkok");
