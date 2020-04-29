771c94192a9555466fbd8a8cb00bd9d52ba70067
nattaska <natt.ska@gmail.com>
2020-04-29 14:50:19 +0700

    Fix find ip

--- controllers/checkin.php
+++ controllers/checkin.php
@@ -5,7 +5,19 @@ class Checkin extends Controller {
         parent::__construct();
         Session::init();
 
-        $ip=$_SERVER['REMOTE_ADDR'];
+        if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+        {
+            $ip=$_SERVER['HTTP_CLIENT_IP'];
+        }
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+        {
+            $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        else
+        {
+            $ip=$_SERVER['REMOTE_ADDR'];
+        }
+        
         $ipinfo = file_get_contents("http://ipinfo.io/{$ip}");
         echo $ipinfo."</br>";
         
