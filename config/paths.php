c7cf7935ae8e8a1fec8c0588d9112abccead0171
nattaska <natt.ska@gmail.com>
2020-08-13 13:26:49 +0700

    Update paths.php

--- config/paths.php
+++ config/paths.php
@@ -2,7 +2,7 @@
 
 // define('URL', 'https://localhost/kkm/');
 define('URL', 'https://kkmerp.herokuapp.com/');
-define('NT_ORG', 'AS17552 True Internet');
+define('NT_ORG', 'AS17552 Asia Pacific Network Information Centre');
 define('NT_HOST', 'asianet.co.th');
 date_default_timezone_set("Asia/Bangkok");
 
