aeeaeaf5a0be19b5f991f7edd4b5488d4134ff93
nattaska <natt.ska@gmail.com>
2020-10-19 17:07:19 +0700

    Commit

--- update_ip.php
+++ update_ip.php
@@ -11,7 +11,6 @@ $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
 $ins_hist = "INSERT INTO ip_history(ip_addr) VALUES(:ip)";
-$stmt_hist = $db->prepare($ins_hist);
 
 $upd_param = "UPDATE prmdtl
         SET pmdval1=:ip
@@ -19,9 +18,21 @@ $upd_param = "UPDATE prmdtl
         WHERE pmdtbno=1
         AND pmdcd=3";
 
-$stmt_upd = $db->prepare($upd_param);
+try {
+    $this->db->beginTransaction();
 
-$stmt_hist->execute(array(':ip'=>$ip));
-$stmt_upd->execute(array(':ip'=>$ip));
+    $stmt_hist = $db->prepare($ins_hist);
+    $stmt_upd = $db->prepare($upd_param);
+
+    $stmt_hist->execute(array(':ip'=>$ip));
+    $stmt_upd->execute(array(':ip'=>$ip));
+
+    $db->commit();
+
+} catch (Exception $e) {
+    $result = "0";
+    $error = $e->getMessage();
+    $db->rollBack();
+}
 
 ?>
