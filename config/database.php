0efa77f7d77373ee06aba2a6b2f1ef0709ab4f11
nattaska <natt.ska@gmail.com>
2020-11-01 17:46:17 +0700

    Config

--- config/database.php
+++ config/database.php
@@ -1,14 +1,14 @@
 <?php 
 
 define('DB_TYPE', 'mysql');
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'kkm');
-define('DB_USER', 'kruakroomeuk');
-define('DB_PASS', '120360');
-// define('DB_HOST', 'remotemysql.com');
-// define('DB_NAME', 'IaMuYzAimD');
-// define('DB_USER', 'IaMuYzAimD');
-// define('DB_PASS', 'RFJPNT0kiu');
+// define('DB_HOST', '127.0.0.1');
+// define('DB_NAME', 'kkm');
+// define('DB_USER', 'kruakroomeuk');
+// define('DB_PASS', '120360');
+define('DB_HOST', 'remotemysql.com');
+define('DB_NAME', 'IaMuYzAimD');
+define('DB_USER', 'IaMuYzAimD');
+define('DB_PASS', 'RFJPNT0kiu');
 define('DB_CHARSET', 'utf8');
 
 ?>
--- config/paths.php
+++ config/paths.php
@@ -1,7 +1,7 @@
 <?php
 
-define('URL', 'http://localhost/kkm/');
-// define('URL', 'https://kkmerp.herokuapp.com/');
+// define('URL', 'https://localhost/kkm/');
+define('URL', 'https://kkmerp.herokuapp.com/');
 define('NT_ORG', 'AS17552 True Internet Co.,Ltd.');
 // define('NT_ORG', 'AS17552 Asia Pacific Network Information Centre');
 define('NT_HOST', 'asianet.co.th');
