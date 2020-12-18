d5406c0e1d5494f4ab92ad47677ce3004a7ee6ed
nattaska <natt.ska@gmail.com>
2020-12-18 20:24:46 +0700

    Line user profile

--- line/index.php
+++ line/index.php
@@ -13,6 +13,11 @@ $replyToken = $deCode['events'][0]['replyToken'];
 $userId = $deCode['events'][0]['source']['userId'];
 $text = $deCode['events'][0]['message']['text'];
 
+$LINEDatas['url'] = "https://api.line.me/v2/bot/profile/".$userId;
+$LINEDatas['token'] = CHANNEL_ACCESS_TOKEN;
+$userProfile = getLINEProfile($LINEDatas);
+file_put_contents('log-profile.txt', $userProfile['message'] . PHP_EOL, FILE_APPEND);
+
 $replyMessages = [];
 $replyMessages['replyToken'] = $replyToken;
 $replyMessages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
@@ -74,6 +79,42 @@ function sentMessage($encodeJson,$datas) {
 
     return $datasReturn;
 }
+
+function getLINEProfile($datas)
+{
+   $datasReturn = [];
+   $curl = curl_init();
+   curl_setopt_array($curl, array(
+     CURLOPT_URL => $datas['url'],
+     CURLOPT_RETURNTRANSFER => true,
+     CURLOPT_ENCODING => "",
+     CURLOPT_MAXREDIRS => 10,
+     CURLOPT_TIMEOUT => 30,
+     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+     CURLOPT_CUSTOMREQUEST => "GET",
+     CURLOPT_HTTPHEADER => array(
+       "Authorization: Bearer ".$datas['token'],
+       "cache-control: no-cache"
+     ),
+   ));
+   $response = curl_exec($curl);
+   $err = curl_error($curl);
+   curl_close($curl);
+   if($err){
+      $datasReturn['result'] = 'E';
+      $datasReturn['message'] = $err;
+   }else{
+      if($response == "{}"){
+          $datasReturn['result'] = 'S';
+          $datasReturn['message'] = 'Success';
+      }else{
+          $datasReturn['result'] = 'E';
+          $datasReturn['message'] = $response;
+      }
+   }
+   return $datasReturn;
+}
+
 // require '../config/database.php';
 //  $db = new Database();
  
