3f1ac6ecc7abf7f3fcfd476a8ab2ba23a5f76a47
nattaska <natt.ska@gmail.com>
2021-09-06 14:25:17 +0700

    Chang database

--- config/database.php
+++ config/database.php
@@ -7,10 +7,10 @@ define('DB_TYPE', 'mysql');
 // define('DB_USER', 'kruakroomeuk');
 // define('DB_PASS', '120360');
 
-define('DB_HOST', 'remotemysql.com');
-define('DB_NAME', 'IaMuYzAimD');
-define('DB_USER', 'IaMuYzAimD');
-define('DB_PASS', 'RFJPNT0kiu');
+// define('DB_HOST', 'remotemysql.com');
+// define('DB_NAME', 'IaMuYzAimD');
+// define('DB_USER', 'IaMuYzAimD');
+// define('DB_PASS', 'RFJPNT0kiu');
 
 // define('DB_HOST', 'db4free.net');
 // define('DB_NAME', 'kkm_120360');
@@ -21,6 +21,11 @@ define('DB_PASS', 'RFJPNT0kiu');
 // define('DB_NAME', 'sql5426413');
 // define('DB_USER', 'sql5426413');
 // define('DB_PASS', 'MDfTqSVE2x');
+
+define('DB_HOST', 'fdb32.awardspace.net');
+define('DB_NAME', '3933924_kkm');
+define('DB_USER', '3933924_kkm');
+define('DB_PASS', 'kroomeuk120360');
 
 define('DB_CHARSET', 'utf8');
 
