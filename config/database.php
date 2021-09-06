0dc691b243e3f6c5b2150cf6530c484493d3ef3d
nattaska <natt.ska@gmail.com>
2021-09-06 14:27:40 +0700

    Chang database

--- config/database.php
+++ config/database.php
@@ -12,20 +12,20 @@ define('DB_TYPE', 'mysql');
 // define('DB_USER', 'IaMuYzAimD');
 // define('DB_PASS', 'RFJPNT0kiu');
 
-// define('DB_HOST', 'db4free.net');
-// define('DB_NAME', 'kkm_120360');
-// define('DB_USER', 'kruakroomeuk');
-// define('DB_PASS', 'kroomeuk120360');
+define('DB_HOST', 'db4free.net');
+define('DB_NAME', 'kkm_120360');
+define('DB_USER', 'kruakroomeuk');
+define('DB_PASS', 'kroomeuk120360');
 
 // define('DB_HOST', 'sql5.freemysqlhosting.net');
 // define('DB_NAME', 'sql5426413');
 // define('DB_USER', 'sql5426413');
 // define('DB_PASS', 'MDfTqSVE2x');
 
-define('DB_HOST', 'fdb32.awardspace.net');
-define('DB_NAME', '3933924_kkm');
-define('DB_USER', '3933924_kkm');
-define('DB_PASS', 'kroomeuk120360');
+// define('DB_HOST', 'fdb32.awardspace.net');
+// define('DB_NAME', '3933924_kkm');
+// define('DB_USER', '3933924_kkm');
+// define('DB_PASS', 'kroomeuk120360');
 
 define('DB_CHARSET', 'utf8');
 
