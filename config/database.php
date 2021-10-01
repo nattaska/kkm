b11fa8e145e8cc7ca2e2379166664f48f62ae050
nattaska <natt.ska@gmail.com>
2021-10-01 16:41:36 +0700

    Chang database

--- config/database.php
+++ config/database.php
@@ -7,15 +7,15 @@ define('DB_TYPE', 'mysql');
 // define('DB_USER', 'kruakroomeuk');
 // define('DB_PASS', '120360');
 
-// define('DB_HOST', 'remotemysql.com');
-// define('DB_NAME', 'IaMuYzAimD');
-// define('DB_USER', 'IaMuYzAimD');
-// define('DB_PASS', 'RFJPNT0kiu');
+define('DB_HOST', 'remotemysql.com');
+define('DB_NAME', 'IaMuYzAimD');
+define('DB_USER', 'IaMuYzAimD');
+define('DB_PASS', 'RFJPNT0kiu');
 
-define('DB_HOST', 'db4free.net');
-define('DB_NAME', 'kkm_120360');
-define('DB_USER', 'kruakroomeuk');
-define('DB_PASS', 'kroomeuk120360');
+// define('DB_HOST', 'db4free.net');
+// define('DB_NAME', 'kkm_120360');
+// define('DB_USER', 'kruakroomeuk');
+// define('DB_PASS', 'kroomeuk120360');
 
 // define('DB_HOST', 'sql5.freemysqlhosting.net');
 // define('DB_NAME', 'sql5426413');
