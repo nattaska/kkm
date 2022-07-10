944993a9a328254f2d3e9dffe7dcd2dd03ddfc3b
nattaska <natt.ska@gmail.com>
2022-07-10 13:04:18 +0700

    db4free

--- config/database.php
+++ config/database.php
@@ -4,23 +4,23 @@ define('DB_TYPE', 'mysql');
 
 // define('DB_HOST', '127.0.0.1');
 // define('DB_NAME', 'kkm');
-// define('DB_USER', 'kruakroomeuk');
-// define('DB_PASS', '120360');
+// define('DB_USER', 'kkm');
+// define('DB_PASS', 'kkmlocal');
 
-define('DB_HOST', 'remotemysql.com');
-define('DB_NAME', 'IaMuYzAimD');
-define('DB_USER', 'IaMuYzAimD');
-define('DB_PASS', 'RFJPNT0kiu');
+// define('DB_HOST', 'remotemysql.com');
+// define('DB_NAME', 'IaMuYzAimD');
+// define('DB_USER', 'IaMuYzAimD');
+// define('DB_PASS', 'RFJPNT0kiu');
 
-// define('DB_HOST', 'db4free.net');
-// define('DB_NAME', 'kkm_120360');
-// define('DB_USER', 'kruakroomeuk');
-// define('DB_PASS', 'kroomeuk120360');
+define('DB_HOST', 'db4free.net');
+define('DB_NAME', 'kkm_120360');
+define('DB_USER', 'kruakroomeuk');
+define('DB_PASS', 'kroomeuk120360');
 
-// define('DB_HOST', 'sql5.freemysqlhosting.net');
-// define('DB_NAME', 'sql5426413');
-// define('DB_USER', 'sql5426413');
-// define('DB_PASS', 'MDfTqSVE2x');
+// define('DB_HOST', 'sql6.freemysqlhosting.net');
+// define('DB_NAME', 'sql6505169');
+// define('DB_USER', 'sql6505169');
+// define('DB_PASS', 'Gk1bIzfa9H');
 
 // define('DB_HOST', 'fdb32.awardspace.net');
 // define('DB_NAME', '3933924_kkm');
