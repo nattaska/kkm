2ed4002f555cb4170ee564b18417850c4acb3f79
nattaska <natt.ska@gmail.com>
2023-01-10 01:37:27 +0700

    Change DB

--- config/database.php
+++ config/database.php
@@ -17,15 +17,20 @@ define('DB_TYPE', 'mysql');
 // define('DB_USER', 'kruakroomeuk');
 // define('DB_PASS', 'kroomeuk120360');
 
-define('DB_HOST', 'sql6.freemysqlhosting.net');
-define('DB_NAME', 'sql6589252');
-define('DB_USER', 'sql6589252');
-define('DB_PASS', 'HRJRIEvfHI');
+// define('DB_HOST', 'sql6.freemysqlhosting.net');
+// define('DB_NAME', 'sql6589252');
+// define('DB_USER', 'sql6589252');
+// define('DB_PASS', 'HRJRIEvfHI');
 
 // define('DB_HOST', 'fdb32.awardspace.net');
 // define('DB_NAME', '3933924_kkm');
 // define('DB_USER', '3933924_kkm');
 // define('DB_PASS', 'kroomeuk120360');
+
+define('DB_HOST', 'sql.freedb.tech');
+define('DB_NAME', 'freedb_kkmerp');
+define('DB_USER', 'freedb_kkmerp');
+define('DB_PASS', 'nSpbRF6%W$5a6U6');
 
 define('DB_CHARSET', 'utf8');
 
